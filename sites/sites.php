69a886deced7c0c07518e6507ff46581f8c11a4e
stratosys <ashish@logbullit.com>
2016-09-07 10:38:54 +0000

    First commit while creating private repo

--- sites/sites.php
+++ sites/sites.php
@@ -54,4 +54,4 @@
  * @see http://drupal.org/documentation/install/multi-site
  */
 
-$sites['dev-evolver0709c1s3-f7338a34.evolverinc.tech'] = 'all';
+$sites['test-evolver0709c1s3-f7338a34.evolverinc.tech'] = 'all';
